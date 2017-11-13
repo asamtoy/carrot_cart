1cda155f49a46a91ee225d825bff73ec8614b3ed
Andrew Samtoy <asamtoy@gmail.com>
2017-11-13 10:28:23 +0000

    added API with seed plants

---
+++ carrot_cart_2.0/client/app.js
@@ -0,0 +1,26 @@
+// var requestHelper = require('./helpers/request_helper.js')
+// var geolocation = require('./helpers/geolocation.js')
+
+var initialize = function(){
+  var geolocation = {
+    enableHighAccuracy: false,
+    timeout: 5000,
+    maximumAge: 0
+    };
+
+    function success(pos) {
+    var crd = pos.coords;
+
+    console.log(crd);
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+
+    function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, geolocation);
+  }
+}
+window.addEventListener('load', initialize);
---
+++ carrot_cart_2.0/server/db/seed_vegetables.js
@@ -0,0 +1,110 @@
+db.dropDatabase();
+
+db.plants.insert([
+
+  {
+    name: "Beet",
+    mintemp: 4
+  },
+
+{
+  name: "celery",
+  mintemp: 4
+},
+
+{
+  name: "carrot",
+  mintemp: 4
+},
+
+{
+  name: "cabbage",
+  mintemp: 4
+},
+
+{
+  name: "cauliflower",
+  mintemp: 4
+},
+
+{
+  name: "broccoli",
+  mintemp: 4
+},
+
+{
+  name: "cucumber",
+  mintemp: 15
+},
+
+{
+  name: "eggplant",
+  mintemp: 15
+},
+
+{
+  name: "lettuce",
+  mintemp: 0
+},
+
+{
+  name: "melon",
+  mintemp: 15
+},
+
+{
+  name: "onion",
+  mintemp: 0
+},
+
+{
+  name: "pea",
+  mintemp: 4
+},
+
+{
+  name: "pepper",
+  mintemp: 15
+},
+
+{
+  name: "pumpkin",
+  mintemp: 15
+},
+
+{
+  name: "radish",
+  mintemp: 4
+},
+
+{
+  name: "spinach",
+  mintemp: 0
+},
+
+{
+  name: "sweet corn",
+  mintemp: 10
+},
+
+{
+  name: "swiss chard",
+  mintemp: 4
+},
+
+{
+  name: "tomato",
+  mintemp: 10
+},
+
+{
+  name: "turnip"
+  mintemp: 15
+},
+
+{
+  name: "rutabega",
+  mintemp: 15
+}
+
+])
